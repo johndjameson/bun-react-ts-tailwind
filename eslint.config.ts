6ebc7e733270048d910aca823d4c486d774bac94
John D. Jameson <thejohnjameson@gmail.com>
2025-08-09 17:40:02 -0500

    Add hooks eslint plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import eslint from "@eslint/js";
 import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -10,8 +11,8 @@ export default tseslint.config(
   // @ts-expect-error https://github.com/jsx-eslint/eslint-plugin-react/issues/3878
   react.configs.flat.recommended,
   react.configs.flat["jsx-runtime"],
+  reactHooks.configs["recommended-latest"],
   {
-    plugins: { react },
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
