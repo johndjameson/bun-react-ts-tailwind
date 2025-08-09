580dee46ef2f853f335e1de87d92e2f389bf626c
John D. Jameson <thejohnjameson@gmail.com>
2025-08-09 17:20:53 -0500

    Fix lint errors

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -9,6 +9,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "@/App";
 
 function start() {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const root = createRoot(document.getElementById("root")!);
   root.render(<App />);
 }
