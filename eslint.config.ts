032896251c903537eb2ef12fb5c0013ff1a2986b
John D. Jameson <thejohnjameson@gmail.com>
2025-08-09 17:01:23 -0500

    Update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
+import eslint from "@eslint/js";
+import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import globals from "globals";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylistic,
+  // @ts-expect-error https://github.com/jsx-eslint/eslint-plugin-react/issues/3878
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  {
+    plugins: { react },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: "./tsconfig.json",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+);
